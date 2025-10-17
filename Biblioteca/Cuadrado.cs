c1a3af35d55abc6eb105fe214ce4d8c8e70ddca7
p-p <ansh.07qp@gmail.com>
2025-10-17 10:44:56 -0500

    Add files via upload

---
+++ Biblioteca/Cuadrado.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Biblioteca
+{
+    public class Cuadrado
+    {
+        public void area (int lado) 
+        {
+            Console.WriteLine ("\nÁrea:"+lado*lado);
+        }
+
+        public void perimetro(int lado)
+        {
+            Console.WriteLine ("\nPerímetro:"+4*lado);
+        }
+    }
+}
---
+++ Biblioteca/Triangulo.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Biblioteca
+{
+    public class Triangulo
+    {
+        public void area()
+        {
+            Console.Write("\nIngrese la base:");
+            int b = int.Parse(Console.ReadLine());
+            Console.Write("\nIngrese la altura:");
+            int h = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("\nÁrea:" + (b - h) / 2);
+        }
+
+        public void perimetro()
+        {
+            Console.Write("\nIngrese lado1:");
+                int lado1 = int.Parse(Console.ReadLine());
+            Console.Write("\nIngrese lado2:");
+            int lado2 = int.Parse(Console.ReadLine());
+            Console.Write("\nIngrese lado3:");
+            int lado3 = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("\nPerímetro:" + (lado1+lado2+lado3));
+        }
+    }
+}
